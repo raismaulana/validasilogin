b0632f4f9286513546580a0bdc7a5a3d21185b67
Lufri Rais Maulana <lufriraismaulana@gmail.com>
2020-10-09 10:00:18 +0700

    Initial commit

--- app/src/main/java/com/lufriraismaulana/validasilogin/SplashScreenActivity.java
+++ app/src/main/java/com/lufriraismaulana/validasilogin/SplashScreenActivity.java
@@ -2,13 +2,42 @@ package com.lufriraismaulana.validasilogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.io.File;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    public static final String FILENAME = "login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isLogin()){
+                    startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                }else {
+                    Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+                finish();
+            }
+        },3000);
+    }
+    boolean isLogin(){
+        File sdcard = getFilesDir();
+        File file = new File(sdcard, FILENAME);
+        if (file.exists()){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
